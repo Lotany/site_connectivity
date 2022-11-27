98199895a63c509de8d51bde31e2410c9cec5cfb
Able <ablekilinda@gmail.com>
2022-11-28 02:37:19 +0300

    adding a cli interface

--- rpchecker/cli.py
+++ rpchecker/cli.py
@@ -15,4 +15,21 @@ def read_user_cli_args():
         default=[],
         help="Enter one or more wbsite URLs",
     )
+
+    parser.add_argument(
+        "-f",
+        "--input-file",
+        metavar= "FILE",
+        type=str,
+        default="",
+        help="read URLs from a file",
+    )
     return parser.parse_args()
+
+    def display_check_result(result,url,error=""):
+        """Display the result of a connectivity check."""
+        print(f'The status of "{url}" is:',end=" ")
+        if result:
+            print('"Online!" 1')
+        else:
+            print(f'"Offline?" 0 \n Error: "{error}"')
