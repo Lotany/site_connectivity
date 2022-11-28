9e3c3a0d28b24ddcf205f555354de9201487685d
Able <ablekilinda@gmail.com>
2022-11-28 03:13:55 +0300

    Testing my app this is first final commit

--- rpchecker/cli.py
+++ rpchecker/cli.py
@@ -26,10 +26,10 @@ def read_user_cli_args():
     )
     return parser.parse_args()
 
-    def display_check_result(result,url,error=""):
-        """Display the result of a connectivity check."""
-        print(f'The status of "{url}" is:',end=" ")
-        if result:
-            print('"Online!" 1')
-        else:
-            print(f'"Offline?" 0 \n Error: "{error}"')
+def display_check_result(result,url,error=""):
+    """Display the result of a connectivity check."""
+    print(f'The status of "{url}" is:',end=" ")
+    if result:
+        print('"Online!" 1')
+    else:
+        print(f'"Offline?" 0 \n Error: "{error}"')
